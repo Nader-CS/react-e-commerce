479eca2ec0268435c2ee03fcd392628ca8cec0bc
Nader <naderabdalhamed93@gmail.com>
2023-07-30 23:49:46 +0300

    adding footer desing

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,8 +1,14 @@
 import React from "react";
 import classes from "./Footer.module.scss";
 
+const date = new Date();
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={classes.footer}>
+      &copy;{date.getFullYear()} All Rights reserved
+    </div>
+  );
 };
 
 export default Footer;
